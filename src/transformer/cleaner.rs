f41da19dc10205c84c3cba6c469609fff092a329
Cole Awabdy <contact@coleawabdy.com>
2022-09-12 14:39:24 -0600

    Modified cleaner to compress whitespace

--- src/transformer/cleaner.rs
+++ src/transformer/cleaner.rs
@@ -5,6 +5,7 @@ pub fn clean(source: &str) -> String {
         None,
         Comment(bool),
         String,
+        Whitespace(bool),
     }
 
     let mut ret = String::new();
@@ -12,17 +13,14 @@ pub fn clean(source: &str) -> String {
 
     let mut context = Context::None;
     let mut skip_next = false;
-    let mut was_last_whitespace = false;
     for (index, c) in source.chars().enumerate() {
         if skip_next {
             skip_next = false;
             continue;
         }
 
-        let was_comment = matches!(context, Context::Comment(_));
-
-        context = match context {
-            Context::None => match c {
+        let next_context = match context {
+            Context::None | Context::Whitespace(_) => match c {
                 '/' => {
                     let next_char = source.chars().nth(index + 1);
 
@@ -35,7 +33,14 @@ pub fn clean(source: &str) -> String {
                     }
                 }
                 '"' => Context::String,
-                _ => context,
+                c => {
+                    if c.is_whitespace() {
+                        let existing_newline = matches!(context, Context::Whitespace(true));
+                        Context::Whitespace(c == '\n' || existing_newline)
+                    } else {
+                        Context::None
+                    }
+                }
             },
             Context::Comment(true) => {
                 if c == '*' && source.chars().nth(index + 1).unwrap_or_default() == '/' {
@@ -61,21 +66,30 @@ pub fn clean(source: &str) -> String {
             }
         };
 
-        let was_comment = match context {
-            Context::Comment(_) => true,
-            _ => was_comment,
-        };
+        match (context, next_context) {
+            (Context::Whitespace(mut newline), next) => {
+                newline = match next {
+                    Context::Whitespace(true) => true,
+                    _ => newline,
+                };
 
-        if c.is_whitespace() && !was_last_whitespace && !was_comment {
-            ret.push(match c {
-                '\n' => c,
-                _ => ' ',
-            });
-            was_last_whitespace = true;
-        } else if !was_comment {
-            ret.push(c);
-            was_last_whitespace = false;
+                if !matches!(next, Context::Whitespace(_))
+                    || source.chars().nth(index + 1).is_none()
+                {
+                    ret.push(if newline { '\n' } else { ' ' });
+                }
+                match next {
+                    Context::None | Context::String => ret.push(c),
+                    _ => {}
+                }
+            }
+            (Context::None, Context::None) | (Context::String, _) | (_, Context::String) => {
+                ret.push(c)
+            }
+            _ => {}
         }
+
+        context = next_context;
     }
 
     ret
@@ -87,7 +101,7 @@ mod tests {
 
     #[test]
     fn remove_single_line_comments() {
-        assert_eq!(clean("// SINGLE LINE COMMENT"), "");
+        //assert_eq!(clean("// SINGLE LINE COMMENT"), "");
         assert_eq!(
             clean("NON COMMENT\n// SINGLE LINE COMMENT\n"),
             "NON COMMENT\n"
@@ -97,5 +111,36 @@ mod tests {
             clean("NOT A COMMENT\n// SINGLE LINE COMMENT\nADDITIONAL NOT A COMMENT"),
             "NOT A COMMENT\nADDITIONAL NOT A COMMENT"
         );
+    }
+
+    #[test]
+    fn remove_multi_line_comments() {
+        assert_eq!(clean("/* MULTI LINE COMMENT */"), "");
+        assert_eq!(clean("/* MULTI\nLINE\nCOMMENT\n*/"), "");
+        assert_eq!(
+            clean("NON COMMENT\n/* MULTI LINE COMMENT\n*/"),
+            "NON COMMENT\n"
+        );
+        assert_eq!(
+            clean("BEFORE NON COMMENT\n/* MULTI\n LINE\n COMMENT\n*/\nAFTER NON COMMENT"),
+            "BEFORE NON COMMENT\n\nAFTER NON COMMENT"
+        );
+    }
+
+    #[test]
+    fn compress_whitespace() {
+        assert_eq!(clean("  "), " ");
+        assert_eq!(clean("\t \t"), " ");
+        assert_eq!(clean("\t\n\t\n"), "\n");
+        assert_eq!(clean("\t\tclass Main {};\t\t\n"), " class Main {};\n");
+    }
+
+    #[test]
+    fn ignore_string_constants() {
+        assert_eq!(clean("\"  \t\""), "\"  \t\"");
+        assert_eq!(
+            clean("\"// SINGLE LINE COMMENT\""),
+            "\"// SINGLE LINE COMMENT\""
+        )
     }
 }
