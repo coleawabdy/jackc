17a8c449c93dfef9e2b5072cb1d5176448914094
Cole Awabdy <contact@coleawabdy.com>
2022-09-12 10:46:00 -0600

    Add structures to store tokens

--- src/transformer/tokenizer.rs
+++ src/transformer/tokenizer.rs
@@ -1,61 +1,69 @@
+use std::str::FromStr;
+
+use regex::Regex;
+
+lazy_static! {
+    static ref RE_COMMENT_MULTILINE: Regex = Regex::new("/*.|$*/").unwrap();
+}
+
 pub enum Keyword {
-	Class,
-	Method,
-	Function,
-	Constructor,
-	Int,
-	Bool,
-	Char,
-	Void,
-	Var,
-	Static,
-	Field,
-	Let,
-	Do,
-	If,
-	Else,
-	While,
-	Return,
-	True,
-	False,
-	Null,
-	This,
+    Class,
+    Method,
+    Function,
+    Constructor,
+    Int,
+    Bool,
+    Char,
+    Void,
+    Var,
+    Static,
+    Field,
+    Let,
+    Do,
+    If,
+    Else,
+    While,
+    Return,
+    True,
+    False,
+    Null,
+    This,
 }
 
 pub enum Direction {
-	Left,
-	Right,
+    Left,
+    Right,
 }
 
 pub enum Operator {
-	Add,
-	Subtract,
-	Multiply,
-	Divide,
-	And,
-	Or,
-	Not,
-	Lesser,
-	Greater,
+    Add,
+    Subtract,
+    Multiply,
+    Divide,
+    And,
+    Or,
+    Not,
+    Lesser,
+    Greater,
 }
 
 pub enum Symbol {
-	Parenthese(Direction),
-	Bracket(Direction),
-	Brace(Direction),
-	Comma,
-	Semicolon,
-	Equals,
-	Period,
-	Operator(Operator),
+    Parenthese(Direction),
+    Bracket(Direction),
+    Brace(Direction),
+    Comma,
+    Semicolon,
+    Equals,
+    Period,
+    Operator(Operator),
 }
 
 pub enum Token {
-	Keyword(Keyword),
-	Identifier,
-	Symbol,
-	String(String),
-	Integer(u16),
+    Keyword(Keyword),
+    Identifier,
+    Symbol,
+    String(String),
+    Integer(u16),
 }
 
 pub fn tokenize(source: &str) -> Vec<Token> {
@@ -64,5 +72,4 @@ pub fn tokenize(source: &str) -> Vec<Token> {
 
 #[cfg(test)]
 mod tests {
-
 }
