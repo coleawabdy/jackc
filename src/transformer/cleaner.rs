7b59d833002329bcef93a7b81b0ee04cd595ccec
Cole Awabdy <contact@coleawabdy.com>
2022-10-17 09:41:51 -0600

    Finished cleaner implementation

--- src/transformer/cleaner.rs
+++ src/transformer/cleaner.rs
@@ -13,7 +13,9 @@ lazy_static! {
 		|
 		(?P<none>\S+)"#
     )
-    .expect("Failed to build regex");
+    .expect("Failed to build annotation regex");
+    static ref RE_WHITESPACE_COMPRESS: Regex =
+        Regex::new(r"(?m)(?P<ws>\s{2,})").expect("Failed to build whitespace compression regex");
 }
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -46,13 +48,40 @@ fn annotate(source: &str) -> Vec<(&str, Option<Annotation>)> {
 }
 
 #[allow(dead_code)]
-pub fn clean(_source: &str) -> String {
-    todo!()
+pub fn clean(source: &str) -> String {
+    let mut cleaned_src = String::new();
+    cleaned_src.reserve(source.len());
+
+    let mut last_annotation: Option<Annotation> = None;
+    for annotation in annotate(source) {
+        cleaned_src += match annotation.1 {
+            None | Some(Annotation::String) => {
+                last_annotation = annotation.1;
+                annotation.0
+            }
+            Some(Annotation::Comment) => "",
+            Some(Annotation::Whitespace) => {
+                if last_annotation != Some(Annotation::Whitespace) {
+                    last_annotation = Some(Annotation::Whitespace);
+                    " "
+                } else {
+                    ""
+                }
+            }
+        }
+    }
+
+    // Remove trailing whitespace
+    if last_annotation == Some(Annotation::Whitespace) {
+        cleaned_src.remove(cleaned_src.len() - 1);
+    }
+
+    cleaned_src
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{annotate, Annotation};
+    use super::{annotate, clean, Annotation};
 
     #[test]
     fn annotate_string() {
@@ -112,12 +141,24 @@ mod tests {
         )
     }
 
-    /*#[test]
+    #[test]
+    fn annotate_composite() {
+        assert_eq!(
+            annotate("\"// COMMENT\""),
+            vec![("\"// COMMENT\"", Some(Annotation::String))]
+        );
+        assert_eq!(
+            annotate("/*\"INNTER STRING\"*/"),
+            vec![("/*\"INNTER STRING\"*/", Some(Annotation::Comment))]
+        );
+    }
+
+    #[test]
     fn remove_single_line_comments() {
         assert_eq!(clean("// SINGLE LINE COMMENT"), "");
         assert_eq!(
             clean("NON COMMENT\n// SINGLE LINE COMMENT\n"),
-            "NON COMMENT "
+            "NON COMMENT"
         );
         assert_eq!(clean("NOT A COMMENT"), "NOT A COMMENT");
         assert_eq!(
@@ -128,28 +169,42 @@ mod tests {
 
     #[test]
     fn remove_multi_line_comments() {
-        assert_eq!(clean("/* MULTI LINE COMMENT */
-    "), "");
-        assert_eq!(clean(" /* MULTI\nLINE\nCOMMENT\n*/
-    "), "");
         assert_eq!(
-            clean("NON COMMENT\n /* MULTI LINE COMMENT\n*/
-    "),
-            "NON COMMENT "
+            clean(
+                "/* MULTI LINE COMMENT */
+        "
+            ),
+            ""
         );
         assert_eq!(
-            clean("BEFORE NON COMMENT\n /* MULTI\n LINE\n COMMENT\n*/
-    \nAFTER NON COMMENT"),
+            clean(
+                " /* MULTI\nLINE\nCOMMENT\n*/
+    "
+            ),
+            ""
+        );
+        assert_eq!(
+            clean(
+                "NON COMMENT\n /* MULTI LINE COMMENT\n*/
+    "
+            ),
+            "NON COMMENT"
+        );
+        assert_eq!(
+            clean(
+                "BEFORE NON COMMENT\n /* MULTI\n LINE\n COMMENT\n*/
+    \nAFTER NON COMMENT"
+            ),
             "BEFORE NON COMMENT AFTER NON COMMENT"
         );
     }
 
     #[test]
     fn compress_whitespace() {
-        assert_eq!(clean("  "), " ");
-        assert_eq!(clean("\t \t"), " ");
-        assert_eq!(clean("\t\n\t\n"), " ");
-        assert_eq!(clean("\t\tclass Main {};\t\t\n"), " class Main {}; ");
+        assert_eq!(clean("  "), "");
+        assert_eq!(clean("\t \t"), "");
+        assert_eq!(clean("\t\n\t\n"), "");
+        assert_eq!(clean("\t\tclass Main {};\t\t\n"), " class Main {};");
     }
 
     #[test]
@@ -159,5 +214,5 @@ mod tests {
             clean("\"// SINGLE LINE COMMENT\""),
             "\"// SINGLE LINE COMMENT\""
         )
-    }*/
+    }
 }
